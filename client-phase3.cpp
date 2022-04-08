c293b898bb9a1f42a05d2d0298a3f61b113d77af
govindsaju <govindsaju2002@gmail.com>
2022-04-08 13:39:58 +0530

    buggy version of phase 3

--- client-phase3.cpp
+++ client-phase3.cpp
@@ -20,7 +20,9 @@ private:
     bool depth1 ;
     char rcvmsg[MAXDATASIZE];
     vector<string> recieved_messages ;
+    
 public:
+    vector<string> files_to_ask;
     Neighbour(int _clientid,int _listening_port);
     bool makeConnection();
     bool makeSocket();
@@ -33,10 +35,25 @@ public:
     string rcvMessage();
     string getMessage(int i) ;
     void setUniqueID(int id) ;
-    int GetUniqueID() ;
+    int getUniqueID() ;
     int getPort();
+    void addFileToAsk(string file);
+    string getFileToAsk();
     ~Neighbour();
 };
+
+void Neighbour::addFileToAsk(string file){
+    files_to_ask.push_back(file);
+}
+
+string Neighbour::getFileToAsk(){
+    string r = "";
+    for (auto &u:files_to_ask){
+        r+=u;
+        r+=" ";
+    }
+    return r;
+}
 
 Neighbour::Neighbour(int _clientid,int _listening_port)
 {
@@ -131,7 +148,8 @@ string Neighbour::rcvMessage()
         {
             res += rcvmsg[i];
         }
-        recieved_messages.push_back(res) ;
+        //recieved_messages.push_back(res) ;
+        //Commented this out, as it will cause memory issues for large files
         return res;
     }
 }
@@ -162,7 +180,7 @@ bool Neighbour::compareNeighbours(Neighbour n1, Neighbour n2)
     return (n1.uniqueid < n2.uniqueid);
 }
 
-int Neighbour::GetUniqueID(){
+int Neighbour::getUniqueID(){
     return uniqueid ;
 }
 
@@ -172,7 +190,96 @@ bool compareFunction(Neighbour n1, Neighbour n2)
 }
 
 
-void incoming_threadfn(int sock_fd, int clientid, int uniqueid,vector<pair<string,vector<Neighbour>>> files,vector<string> files_owned)
+
+bool send_file(int new_fd, string path, string filename)
+{
+    //Protocol
+    //p1: filename + nofile     filename + yesfile + length_of_file + numpackets
+    //filepackets: filename + serialno + numbytes + payload
+    //ack: yes + serialno + filename
+    fstream fin(path);
+    string packet;
+    if(!fin.is_open())
+    {
+        packet = filename + " nofile";
+        send(new_fd,packet.c_str(),packet.length(),0);
+        return false;
+    }
+    int length;
+    fin.seekg(0,ios::end);
+    length = fin.tellg();
+    fin.seekg(0,ios::beg);
+
+    packet = filename + " yesfile";
+    packet = packet + " " + to_string(length);
+    int maxpacketpayload = MAXDATASIZE - 100;
+    int numpackets = ceil(double(length)/double(maxpacketpayload));
+    packet = packet + " " + to_string(numpackets);
+    send(new_fd,packet.c_str(),packet.length(),0);
+    int bytes_sent = 0;
+    char rcvmsg[MAXDATASIZE];
+    int serialno = 0;
+    
+    while(fin.good())
+    {
+        packet = "";
+        packet += filename + " " + to_string(serialno);
+        
+        fin.read(rcvmsg,maxpacketpayload);
+        int numbytes = fin.gcount();
+        packet += " " + to_string(numbytes) + " ";
+        for (int i=0;i<numbytes;i++)
+        {
+            packet += rcvmsg[i];
+        }
+        bytes_sent += numbytes;
+        bool acked = false;
+        int maxtries = 16;
+        int tries = 0;
+        while(!acked)
+        {
+            tries++;
+            if (tries > maxtries) return false;
+            send(new_fd,packet.c_str(),packet.length(),0);
+            int numbytes = recv(new_fd,rcvmsg,MAXDATASIZE-1,0);
+            int timeout = 0;
+            if(numbytes == -1)
+            {
+                perror("Could not recieve");
+                acked = false;
+                sleep(timeout);
+                timeout++;
+            }
+            else
+            {
+                string res = "";
+                for (int i=0;i<numbytes;i++)
+                {
+                    res += rcvmsg[i];
+                }
+                if (res == "yes " + to_string(serialno) + " " + filename)
+                {
+                    acked = true;
+                    serialno++;
+                }
+                else
+                {
+                    sleep(timeout);
+                    timeout++;
+                }
+                
+            }
+        }
+
+    }
+
+
+    return true;
+
+}
+
+
+void incoming_threadfn(int sock_fd, int clientid, int uniqueid,vector<string> files_owned, string path)
 {
     struct sockaddr_storage their_addr; // connector's address information
     socklen_t sin_size;
@@ -180,6 +287,8 @@ void incoming_threadfn(int sock_fd, int clientid, int uniqueid,vector<pair<strin
     stringstream ss;
     ss << clientid <<" " << uniqueid;
     string s = ss.str() ;
+    if (path[path.length()-1]!='/')
+        path += "/" ;
     while(true)
     {
         
@@ -212,20 +321,52 @@ void incoming_threadfn(int sock_fd, int clientid, int uniqueid,vector<pair<strin
             // pthread_mutex_unlock(&print_mutex);
             istringstream fil_search_stream(res);
             stringstream file_to_be_sent_stream;
+            vector<pair<string,string>> file_list;
             while(fil_search_stream){
                string  file ;
                fil_search_stream >> file ;
+               
                for(int i = 0 ; i < files_owned.size() ; i++){
                     if(files_owned[i] == file) {
                         file_to_be_sent_stream << file << " " ;
-                        
+                        file_list.push_back({path + file,file});
                     }
                }
             }
             string files_to_send = file_to_be_sent_stream.str() ;
             if (send(new_fd, files_to_send.c_str(), files_to_send.length(), 0) == -1)
                     perror("send");
-                                        
+
+            
+            numbytes =  recv(new_fd, rcvmsg, MAXDATASIZE-1, 0);
+            files_to_send = "" ;
+            if(numbytes == -1)
+            {
+               perror("Could not recieve");
+            }
+            else
+            {
+              for (int i=0;i<numbytes;i++)
+               {
+                   files_to_send += rcvmsg[i];
+               }
+               istringstream file_share_stream(files_to_send);
+                while(file_share_stream){
+                     string  file ;
+                     file_share_stream >> file ;
+                     for(int i = 0 ; i < files_owned.size() ; i++){
+                            if(files_owned[i] == file) {
+                                send_file(new_fd,path + file,file);
+                            }
+                     }
+                }
+
+            }
+            
+            //
+
+
+
             close(new_fd);
             exit(0);
         }
@@ -314,7 +455,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     
-    thread recving(incoming_threadfn,sock_fd,client_id,unique_id,files_needed,files_owned);
+    thread recving(incoming_threadfn,sock_fd,client_id,unique_id,files_owned,argv[2]);
 
 
 
@@ -373,46 +514,27 @@ int main(int argc, char *argv[])
                 
                 
             }
-            /*
-            else if(u.checkDepth1()){
-                cout<<u.rcvMessage()<<endl;
-                string file_given ;
-                istringstream files_rec(u.getMessage(1)) ;
-                int j = 0 ;
-                while(files_rec){
-                    string data ;
-                    files_rec >> data ;
-                    for(int i = 0 ; i < files_needed.size() ; i++){
-                             if(data == files_needed[i].first){
-                                 files_needed[i].second.push_back(u);
-                             }
-                    }
-                    j++ ;
-                }
-                count++ ;
-            }
-            */
+
+            if (allconnected)
+            break;
         }
-        /*
-        if(count == neighbors.size()){
-            for(int i = 0 ; i < files_needed.size() ; i++){
-                    sort(files_needed[i].second.begin(),files_needed[i].second.end(),compareFunction);
-                    if(!files_needed[i].second.empty()) {
-                       cout << "Found " <<  files_needed[i].first <<" at "<<files_needed[i].second[0].GetUniqueID() <<" with MD5 0 at depth 1\n" ;
-                    }
-                    else {
-                       cout << "Found " <<  files_needed[i].first <<" at 0 with MD5 0 at depth 0\n" ;
-                    }
-               }
-        }*/
-        if (allconnected)
-        break;
     }
+
 
     for(int i = 0 ; i < files_needed.size() ; i++){
             sort(files_needed[i].second.begin(),files_needed[i].second.end(),compareFunction);
             if(!files_needed[i].second.empty()) {
-                cout << "Found " <<  files_needed[i].first <<" at "<<files_needed[i].second[0].GetUniqueID() <<" with MD5 0 at depth 1\n" ;
+                //files_needed[i].second contains a copy of the neighbour. For all networking reasons, use the one in neighbors list
+                for (auto &u:neighbors)
+                {
+                    if (u.getUniqueID() == files_needed[i].second[0].getUniqueID())
+                    {
+                        u.addFileToAsk(files_needed[i].first);
+                        break;
+                    }
+                }
+                //cout << "Found " <<  files_needed[i].first <<" at "<<files_needed[i].second[0].GetUniqueID() <<" with MD5 0 at depth 1\n" ;
+
             }
             else {
                 cout << "Found " <<  files_needed[i].first <<" at 0 with MD5 0 at depth 0\n" ;
@@ -420,6 +542,53 @@ int main(int argc, char *argv[])
             //cout<<files_needed[i].first<<" "<<files_needed[i].second.size()<<"\n";
         }
 
+    //p1: filename + nofile     filename + yesfile + length_of_file + numpackets
+    //filepackets: filename + serialno + numbytes + payload
+    //ack: yes + serialno + filename
+    for (auto &u:neighbors)
+    {
+        string req = u.getFileToAsk();
+        u.sendMessage(req);
+        for (int i=0;i<u.files_to_ask.size();i++)
+        {
+            string s = u.rcvMessage();
+            istringstream details(s);
+            string file_name;
+            details >> file_name;
+            string foundfile;
+            details >> foundfile;
+            if (foundfile=="nofile") continue;
+            int length,numpackets;
+            details >> length >> numpackets;
+            string packet;
+            ofstream fil;
+            string filepath(argv[2]);
+            filepath.append("/downloads/");
+            filepath.append(file_name);
+            fil.open(filepath.c_str(),ios::binary);
+            for (int j=0;j<numpackets;j++)
+            {
+                packet = u.rcvMessage();
+                istringstream packet_stream(packet);
+                string filename_rc;
+                int serialno,numbytes;
+                packet_stream >> filename_rc >> serialno >> numbytes;
+                char c;
+                packet_stream.get(c);
+                string payload="";
+                for (int i=0;i<numbytes;i++){
+                    packet_stream.get(c);
+                    payload += c;
+                }
+                
+                fil.write(payload.c_str(),numbytes);
+                u.sendMessage("ack "+to_string(serialno)+" "+filename_rc);
+            }
+            fil.close();
+        }
+
+    }
+    
 
     recving.join();
 
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
+client-phase3.o:client-phase3.cpp
+	g++ client-phase3.cpp -o client-phase3.o -lpthread
 client-phase2.o:client-phase2.cpp
 	g++ client-phase2.cpp -o client-phase2.o -lpthread
 client-phase1.o:client-phase1.cpp
 	g++ client-phase1.cpp -o client-phase1.o -lpthread
-
